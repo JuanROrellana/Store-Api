4a7dc2a056d446974bd5894e79ec2c1a2603f10e
Juan Ramirez <ramirez.orellana.juanjose@gmail.com>
2019-06-16 18:14:33 -0600

    Add Authorization

--- store-api/app/Providers/AuthServiceProvider.php
+++ store-api/app/Providers/AuthServiceProvider.php
@@ -28,5 +28,12 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
         
+        Gate::define('admin-only', function ($user) {
+            if($user->isAdmin == 1)
+            {
+                return true;
+            }
+            return false;
+        });
     }
 }
